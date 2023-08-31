d6ab7fe0b796c390d145e40cdf7447e28ff6b407
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 09:07:47 -0600

    Add info section

--- my-site/src/app/page.js
+++ my-site/src/app/page.js
@@ -8,17 +8,48 @@ export default function Home() {
         <div>
           <h1>
             <span>
-              Hello<span className={styles.yellowText}>.</span>
+              Hola<span className={styles.yellowText}>.</span>
             </span>
             <br />
             <span>
-              <span>I am</span>
+              <span>soy</span>
             </span>
             <br />
-            <span>Josh Carlson</span>
+            <span>Gerardo Gómez</span>
           </h1>
         </div>
       </header>
+      <section className={styles.infosection}>
+        <img
+        src="mi honesta reacion.jpg"
+        alt="imagen"
+        classname={styles.Image}
+        />
+        <div className={styles.infocontainer}>
+          <span className={styles.title + " " + styles.yellowText}>Gerardo</span>
+          <br />
+          <span classname={styles.title}>Gómez</span>
+          </div>
+            <ul>
+            <li>
+              <span className={styles.grayText}>Edad:</span> 17
+            </li>
+            <li>
+              <span className={styles.grayText}>Nacionalidad:</span> Guatemalteco
+            </li>
+            <li>
+            <span className={styles.grayText}>Habilidades:</span> no se va
+            </li>
+            <li>
+            <span className={styles.grayText}>Lenguajes:</span> Español e ingles
+            </li>
+          </ul>
+      </section>
+        <div>
+          <span>Gerado</span>
+          <br />
+          <span>Gomez</span>
+          </div>
     </main>
   );
 }
